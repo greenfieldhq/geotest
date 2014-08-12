f0348a05e716491b28de1b1d785c56f8c3e00a86
Ryan Tremaine <ryan.trem@gmail.com>
2014-08-12 16:55:45 -0400

    Working example of a simple geo search using PostGIS

--- app/models/block_face_line.rb
+++
@@ -1,2 +0,0 @@
-class BlockFaceLine < ActiveRecord::Base
-end
--- db/migrate/20140720144305_create_block_face_lines.rb
+++
@@ -1,8 +0,0 @@
-class CreateBlockFaceLines < ActiveRecord::Migration
-  def change
-    create_table :block_face_lines do |t|
-
-      t.timestamps
-    end
-  end
-end
